36c6232a131967025c90755532402ad302925629
t-sin <shinichi.tanaka45@gmail.com>
2018-02-11 07:25:50 +0900

    nsynth doesn't use sndfile

--- nsynth.nim
+++ nsynth.nim
@@ -1,18 +1,11 @@
 from os import commandLineParams
 
-import sndfile
+import nim-vorbis
 import portaudio
 
 let args = commandLineParams()
 
 if args.len == 1:
-  var
-    info: TINFO
-    sndfile: ptr TSNDFILE
-
-  sndfile = open(args[0], READ, cast[ptr TINFO](info.addr))
-  echo repr(info)
-
   echo repr(Initialize())
 
   echo repr(Terminate())
--- nsynth.nimble
+++ nsynth.nimble
@@ -8,5 +8,5 @@ license       = "GPL-3.0"
 # Dependencies
 
 requires "nim >= 0.17.2"
-requires "nim-sndfile >= 0.1.0"
+requires "vorbis >= 0.1.3"
 requires "nim-portaudio >= 0.1.3"
