886c1397a0efe2d6e8a0cc97a8e73e26e86b3ce5
t-sin <shinichi.tanaka45@gmail.com>
2018-02-11 09:27:41 +0900

    Logs progress for debugging

--- nsynth.nim
+++ nsynth.nim
@@ -12,8 +12,30 @@ if args.len == 1:
     echo "cannot open '" & args[0] & "'"
     quit(1)
 
+  echo "============== show .ogg info ==============="
+  echo "filename: '" & args[0] & "'"
   echo repr(VF.info(vf.addr, -1))
 
+  echo "============== initialize pa  ==============="
   echo repr(PA.Initialize())
 
+  proc streamCallback (inBuf, outBuf: pointer,
+                       framesPerBuf: culong,
+                       timeInfo: ptr TStreamCallbackTimeInfo,
+                       stateusFlags: TStreamCallbackFlags,
+                       userData: pointer): cint =
+    discard
+  # var stream: PStream
+
+  # PA.OpenDefaultStream(
+  #   stream.addr,
+  #   numInputChannels = 0,
+  #   numOutputChannels = 2,
+  #   sampleFormat = sfFloat32,
+  #   sampleRate = 44_100,
+  #   framesPerBuffer = 256,
+  #   streamCallback = cast[pointer](0),
+  #   userData = cast[pointer](0))
+
+  echo "============== terminate pa   ==============="
   echo repr(PA.Terminate())
