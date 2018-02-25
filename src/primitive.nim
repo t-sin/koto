e66f210901193e13fe9a3a3040c785ba74d805e6
t-sin <shinichi.tanaka45@gmail.com>
2018-02-25 11:25:59 +0900

    Generates envelopes...?

--- src/primitive.nim
+++ src/primitive.nim
@@ -16,13 +16,18 @@ type
 
 const seq_step = 16
 type
+  ASDR* = enum
+    None, Attack, Decay, Sustin, Release
   StepSequencer* = ref object
     tempo*: float64
     sequence*: string
     noteDuration*: int
     osc*: wt.WaveTableOcillator
-    bar*: float64
+
+    beat*: float64
     time*: float64
+    state*: ASDR
+    envelope*: float32
 
 type
   TSound* = tuple[sndout: SoundOut, seq: StepSequencer]
@@ -45,16 +50,40 @@ proc playWithPA(s: string) =
       tableDelta = (float32(osc.tableSize) * float32(freq)) / snd.sndout.sampleRate
       timeDelta = 1 / snd.sndout.sampleRate
 
+    echo $(snd.seq.state) & ". " & $(snd.seq.time) & ", " & $(snd.seq.beat)
     for i in 0..<int(snd.sndout.bufferSize):
-      let val = osc.volume * osc.interpolFn(
+      let val = snd.seq.envelope * osc.interpolFn(
         crop(osc.tablePos, float32(osc.tableSize)), osc)
       outBuf[i] = (val, val)
       osc.tablePos = osc.tablePos + tableDelta
 
+      let before_beat = snd.seq.beat
       snd.seq.time = snd.seq.time + timeDelta
-      snd.seq.bar = snd.seq.bar + timeDelta * snd.seq.tempo / 60
-      osc.volume = m.sin(snd.seq.time * 10 * (2 * m.PI))
-    echo $(snd.seq.time) & ", " & $(snd.seq.bar)
+      snd.seq.beat = snd.seq.beat + timeDelta * snd.seq.tempo / 60
+
+      const
+        attack = 0.01
+        decay = 0.1
+        sustin = 0.2
+        release = 0.4
+      if m.floor(snd.seq.beat) - m.floor(before_beat) == 1:
+        snd.seq.state = ASDR.Attack
+        snd.seq.envelope = (snd.seq.beat - m.floor(snd.seq.beat)) / attack
+      elif snd.seq.beat - m.floor(snd.seq.beat) < attack:
+        snd.seq.state = ASDR.Attack
+        snd.seq.envelope = (snd.seq.beat - m.floor(snd.seq.beat)) / attack
+      elif snd.seq.beat - m.floor(snd.seq.beat) < attack + decay:
+        snd.seq.state = ASDR.Decay
+        snd.seq.envelope = 1 - (snd.seq.beat - m.floor(snd.seq.beat) - attack) / decay  + sustin
+      elif snd.seq.beat - m.floor(snd.seq.beat) < attack + decay + sustin:
+        snd.seq.state = ASDR.Sustin
+        snd.seq.envelope = sustin
+      elif snd.seq.beat - m.floor(snd.seq.beat) < attack + decay + sustin + release:
+        snd.seq.state = ASDR.Release
+        snd.seq.envelope = 0
+      elif snd.seq.beat - m.floor(snd.seq.beat) > attack + decay + sustin + release:
+        snd.seq.state = ASDR.None
+        snd.seq.envelope = 0
 
   var
     stream: PStream
@@ -70,7 +99,10 @@ proc playWithPA(s: string) =
       sequence: s,
       noteDuration: 100,
       osc: osc,
-      time: 0)
+      time: 0,
+      beat: 0,
+      state: ASDR.None,
+      envelope: 0)
     snd: TSound = (sndout, stepseq)
 
   osc.waveTable = wt.makeTable(osc, wt.tri)
@@ -87,7 +119,7 @@ proc playWithPA(s: string) =
     userData = cast[pointer](snd.addr))
 
   discard PA.StartStream(stream)
-  PA.Sleep(2000)
+  PA.Sleep(4000)
   discard PA.StopStream(stream)
   discard PA.CloseStream(stream)
 
