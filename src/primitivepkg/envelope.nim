2868058b7b860027be15c5731f54650370c93721
t-sin <shinichi.tanaka45@gmail.com>
2018-02-27 23:37:17 +0900

    Cosmetic change

--- src/primitivepkg/envelope.nim
+++ src/primitivepkg/envelope.nim
@@ -16,18 +16,23 @@ proc noteOn*(env: Envelope, startTime: float32) =
 
 proc generateEnvelope*(env: Envelope, time: float32): float32 =
   let noteTime = time - env.startTime
+
   if env.state in [ASDR.None, ASDR.Attack] and noteTime < env.a:
     env.state = ASDR.Attack
     return noteTime / env.a
+
   elif env.state in [ASDR.Attack, ASDR.Decay] and noteTime < env.a + env.d:
     env.state = ASDR.Decay
     return 1 - (noteTime - env.a) / env.d + env.s
+
   elif env.state in [ASDR.Decay, ASDR.Sustin] and noteTime < env.a + env.d + env.s:
     env.state = ASDR.Sustin
     return env.s
+
   elif env.state in [ASDR.Sustin, ASDR.Release] and noteTime < env.a + env.d + 0.1 + env.r:
     env.state = ASDR.Release
     return (noteTime - env.a- env.d - env.s) / env.r * env.s
+
   elif noteTime > env.a + env.d + env.s + env.r:
     env.state = ASDR.None
     return 0
