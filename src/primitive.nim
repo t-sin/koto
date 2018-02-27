85f3be19d90bbce8b427d5c50559f57bd7998fab
t-sin <shinichi.tanaka45@gmail.com>
2018-02-27 23:36:02 +0900

    Factors out envelope generator

--- src/primitive.nim
+++ src/primitive.nim
@@ -5,6 +5,7 @@ import vorbis/vorbisfile as VF
 import portaudio as PA
 
 import primitivepkg.oscillators.wave_table as wt
+import primitivepkg.envelope as eg
 import primitivepkg.utils
 
 type
@@ -16,36 +17,6 @@ type
 
 const seq_step = 16
 type
-  ASDR* = enum
-    None, Attack, Decay, Sustin, Release
-  Envelope* = ref object
-    a*: float32
-    d*: float32
-    s*: float32
-    r*: float32
-
-    state*: ASDR
-    startTime*: float32
-
-proc generateEnvelope(env: Envelope, time: float32): float32 =
-  let noteTime = time - env.startTime
-  if env.state in [ASDR.None, ASDR.Attack] and noteTime < env.a:
-    env.state = ASDR.Attack
-    return noteTime / env.a
-  elif env.state in [ASDR.Attack, ASDR.Decay] and noteTime < env.a + env.d:
-    env.state = ASDR.Decay
-    return 1 - (noteTime - env.a) / env.d + env.s
-  elif env.state in [ASDR.Decay, ASDR.Sustin] and noteTime < env.a + env.d + env.s:
-    env.state = ASDR.Sustin
-    return env.s
-  elif env.state in [ASDR.Sustin, ASDR.Release] and noteTime < env.a + env.d + 0.1 + env.r:
-    env.state = ASDR.Release
-    return (noteTime - env.a- env.d - env.s) / env.r * env.s
-  elif noteTime > env.a + env.d + env.s + env.r:
-    env.state = ASDR.None
-    return 0
-
-type
   StepSequencer* = ref object
     tempo*: float64
     sequence*: string
@@ -54,7 +25,7 @@ type
 
     beat*: float64
     time*: float64
-    env*: Envelope
+    env*: eg.Envelope
 
 type
   TSound* = tuple[sndout: SoundOut, seq: StepSequencer]
@@ -92,8 +63,7 @@ proc playWithPA(s: string) =
 
       # note on
       if m.floor(snd.seq.beat) - m.floor(before_beat) == 1:
-        snd.seq.env.startTime = snd.seq.time
-        snd.seq.env.state = ASDR.Attack
+        eg.noteOn(snd.seq.env, snd.seq.time)
 
   var
     stream: PStream
