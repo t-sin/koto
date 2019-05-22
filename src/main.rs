26b3760032a131c8855c1c2e73929a87038fb22c
t-sin <shinichi.tanaka45@gmail.com>
2019-05-23 00:55:34 +0900

    WIP: it does not work, maybe because of borrowing of sine?

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,33 @@ use cpal::EventLoop;
 use cpal::SampleFormat;
 use cpal::SampleRate;
 
+mod clock;
+mod unit;
+
+use unit::Unit;
+
 fn main() {
-    let sample_rate = 44100;
+    let sample_rate = 44100.0;
 
     let device = cpal::default_output_device().unwrap();
     let format = cpal::Format {
         channels: 1,
-        sample_rate: SampleRate(sample_rate),
+        sample_rate: SampleRate(sample_rate as u32),
         data_type: SampleFormat::F32
     };
     let event_loop = EventLoop::new();
     let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
 
-    println!("formats: {:?}", format);
     event_loop.play_stream(stream_id);
 
-    let mut ph = 0.0f64;
+    let clock = clock::Clock { tick: 0, sample_rate: sample_rate };
+    let sine = unit::Osc { init_ph: 0.0, ph: 0.0, freq: 880.0 };
 
     event_loop.run(move |_stream_id, stream_data| {
         match stream_data {
             cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
                 for elem in buffer.iter_mut() {
-                    *elem = ph.sin() as f32;
-                    ph += (440.0 / sample_rate as f64) * std::f64::consts::PI;
+                    *elem = sine.calc(&clock) as f32;
                 }
             }
             _ => ()
