3e55373400362df656748b36c9faa147158e02e3
t-sin <shinichi.tanaka45@gmail.com>
2019-06-08 21:34:59 +0900

    Remove unreached codes

--- src/units/sequencer.rs
+++ src/units/sequencer.rs
@@ -27,42 +27,38 @@ impl Signal for ADSREnvelope {
         let eplaced = self.eplaced;
         let v;
 
-        if eplaced < 0 {
-            v = 0.0;
-        } else {
-            match state {
-                ADSR::Attack => {
-                    if eplaced < self.a {
-                        v = self.eplaced as f64 / self.a as f64;
-                    } else if eplaced < self.a + self.d {
-                        v = 1.0 - (1.0 - self.s as f64) * ((eplaced as f64 - self.a as f64) / self.d as f64);
-                    } else {
-                        v = 0.0;
-                    }
-                },
-                ADSR::Decay => {
-                    if eplaced < self.a + self.d {
-                        v = 1.0 - (1.0 - self.s as f64) * ((eplaced as f64 - self.a as f64) / self.d as f64);
-                    } else if eplaced >= self.a + self.d {
-                        v = self.s as f64;
-                    } else {
-                        v = 0.0;
-                    }
-                },
-                ADSR::Sustin => {
-                    v = self.s as f64;
-                },
-                ADSR::Release => {
-                    if eplaced < self.r {
-                        v = self.s as f64 - eplaced as f64 * (self.s as f64 / self.r as f64);
-                    } else {
-                        v = 0.0;
-                    }
-                },
-                ADSR::None => {
+        match state {
+            ADSR::Attack => {
+                if eplaced < self.a {
+                    v = self.eplaced as f64 / self.a as f64;
+                } else if eplaced < self.a + self.d {
+                    v = 1.0 - (1.0 - self.s as f64) * ((eplaced as f64 - self.a as f64) / self.d as f64);
+                } else {
                     v = 0.0;
-                },
-            }
+                }
+            },
+            ADSR::Decay => {
+                if eplaced < self.a + self.d {
+                    v = 1.0 - (1.0 - self.s as f64) * ((eplaced as f64 - self.a as f64) / self.d as f64);
+                } else if eplaced >= self.a + self.d {
+                    v = self.s as f64;
+                } else {
+                    v = 0.0;
+                }
+            },
+            ADSR::Sustin => {
+                v = self.s as f64;
+            },
+            ADSR::Release => {
+                if eplaced < self.r {
+                    v = self.s as f64 - eplaced as f64 * (self.s as f64 / self.r as f64);
+                } else {
+                    v = 0.0;
+                }
+            },
+            ADSR::None => {
+                v = 0.0;
+            },
         }
         (v, v)
     }
@@ -73,37 +69,33 @@ impl Stateful for ADSREnvelope {
         let state = &self.state;
         let eplaced = self.eplaced;
 
-        if eplaced < 0 {
-            self.state = ADSR::None;
-        } else {
-            match state {
-                ADSR::Attack => {
-                    if eplaced < self.a {
-                        ;
-                    } else if eplaced < self.a + self.d {
-                        self.state = ADSR::Decay;
-                    } else {
-                        self.state = ADSR::None;
-                    }
-                },
-                ADSR::Decay => {
-                    if eplaced < self.a + self.d {
-                        ;
-                    } else if eplaced >= self.a + self.d {
-                        self.state = ADSR::Sustin;
-                    } else {
-                        self.state = ADSR::None;
-                    }
-                },
-                ADSR::Sustin => {},
-                ADSR::Release => {
-                    if eplaced < self.r {
-                    } else {
-                        self.state = ADSR::None;
-                    }
-                },
-                ADSR::None => {},
-            }
+        match state {
+            ADSR::Attack => {
+                if eplaced < self.a {
+                    ;
+                } else if eplaced < self.a + self.d {
+                    self.state = ADSR::Decay;
+                } else {
+                    self.state = ADSR::None;
+                }
+            },
+            ADSR::Decay => {
+                if eplaced < self.a + self.d {
+                    ;
+                } else if eplaced >= self.a + self.d {
+                    self.state = ADSR::Sustin;
+                } else {
+                    self.state = ADSR::None;
+                }
+            },
+            ADSR::Sustin => {},
+            ADSR::Release => {
+                if eplaced < self.r {
+                } else {
+                    self.state = ADSR::None;
+                }
+            },
+            ADSR::None => {},
         }
         self.eplaced += 1;
     }
