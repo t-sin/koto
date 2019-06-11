7c6dacae4900ab5ea95fdd09ebf294f4b346517e
t-sin <shinichi.tanaka45@gmail.com>
2019-06-11 23:40:39 +0900

    Restore wave table

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ mod audio_device;
 mod time;
 mod units;
 
+use std::sync::{Arc, Mutex};
+
 use audio_device::AudioDevice;
 use time::Time;
 use time::Clock;
 use units::unit::Signal;
 
 use units::unit::Unit;
-// use units::oscillator::WaveTable;
+use units::oscillator::WaveTable;
 // use units::oscillator::TablePhase;
 
 fn main() {
@@ -18,19 +20,20 @@ fn main() {
 
     let mut time = Time::new(sample_rate, 120.0);
 
-    let s = String::from("(gain 0.1 (offset 0.0 (saw 0.0 440)))");
-    let unit_graph = units::conflisp::eval_one(&units::conflisp::read(s)[0]);
+    let s = String::from("(gain 0.5 (offset 1 (saw 0.0 440)))");
+    let unit_graph1 = units::conflisp::eval_one(&units::conflisp::read(s)[0]);
 
-    // let mut table = Vec::new();
-    // let mut unit_graph;
-    // for i in 0..256 {
-    //     let th = (i as f64) / (256.0) * std::f64::consts::PI * 2.0;
-    //     table.push(th.sin());
-    // }
-    // unit_graph = Unit::Unit(Arc::new(Mutex::new(WaveTable {
-    //     table: table,
-    //     ph: &TablePhase::new(&unit_graph1),
-    // })));
+    let mut table = Vec::new();
+    let mut unit_graph;
+    for i in 0..256 {
+        let th = (i as f64) / (256.0) * std::f64::consts::PI * 2.0;
+        table.push(th.sin());
+    }
+    unit_graph = Arc::new(Mutex::new(
+        Unit::Unit(Arc::new(Mutex::new(WaveTable {
+            table: table,
+            ph: unit_graph1
+    })))));
 
     audio_device.run(|mut buffer| {
         for elem in buffer.iter_mut() {
