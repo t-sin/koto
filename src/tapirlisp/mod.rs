7a7283142d86541b13aad6f4620e7fecc22129bc
t-sin <shinichi.tanaka45@gmail.com>
2019-06-17 18:33:41 +0900

    Avoid borrowing

--- src/tapirlisp/mod.rs
+++ src/tapirlisp/mod.rs
@@ -5,12 +5,12 @@ pub use io::{read, print};
 
 use types::Cons;
 
-pub fn to_vec(list: &Cons) -> Vec<&Cons> {
+pub fn to_vec(list: &Cons) -> Vec<Box<Cons>> {
     match list {
         Cons::Nil => Vec::new(),
         Cons::Cons(elem, rest) => {
-            let mut v: Vec<&Cons> = Vec::new();
-            v.push(elem);
+            let mut v: Vec<Box<Cons>> = Vec::new();
+            v.push(Box::new((**elem).clone()));
             v.append(&mut to_vec(rest));
             v
         },
