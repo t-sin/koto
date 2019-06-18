42b957fa76a61fc3e67a9fb8476739d6c8b66a27
t-sin <shinichi.tanaka45@gmail.com>
2019-06-18 15:44:33 +0900

    Test

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,20 @@ fn main() {
     let mut env = Env::init(time);
 
     let s = r"
-(def $pat (pat ((c 2) (r 2)   (d 2) (r 2)
-                (e 2) (r 2)   (f 2) (r 2)
-                (g 2) (r 2)   (a 2) (r 2)
-                (b 2) (r 2)   (c5 2) (r 2)
+(def $pat1 (pat ((c 1) (r 1) (c 0) (r 0) (r 1) (d 2) (r 2)
+                (e 1) (r 1) (e 0) (r 0) (r 1) (f 2) (r 2)
+                (g 2) (r 2) (a 1) (r 1) (r 1) (r 0) (a 0)
+                (b 2) (r 2) (c5 2) (r 2)
                 loop)))
-(def $osc (wavetable (saw 0 1) (phase (saw 0 440))))
-(def $eg (adsr 0 (gain 0.2 (offset 1 (saw 0 0.25))) 0.0 0.1))
-(seq $pat $osc $eg)
+(def $osc1 (wavetable (saw 0 1) (phase (saw 0 440))))
+(def $eg1 (adsr 0 (gain 0.2 (offset 1 (saw 0 0.25))) 0.0 0.1))
+
+(def $pat2 (pat ((c 2) (r 2) (c 2) (r 2) (c 2) (r 2) (c 2) (r 2) loop)))
+(def $osc2 (rand 0))
+(def $eg2 (adsr 0 0.1 0.005 0))
+
+(+ (gain 0.3 (seq $pat1 $osc1 $eg1))
+   (gain 0.25 (seq $pat2 $osc2 $eg2)))
 ".to_string();
     let unit_graph = match tlisp::eval_all(tlisp::read(s).unwrap(), &mut env) {
         Ok(Value::Unit(ug)) => ug,
