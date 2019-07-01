4be0e45d1e2ae384e3bdf5c596dcc6f5b7bd29a6
t-sin <shinichi.tanaka45@gmail.com>
2019-07-01 22:02:26 +0900

    Global state as a `SoundSystem`

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,48 @@ mod tapirlisp;
 use audio_device::AudioDevice;
 use time::{Time, Clock};
 
-use units::unit::Unit;
+use units::unit::{Unit, AUnit};
 
 use tapirlisp::types::{Value, Env};
 use tapirlisp as tlisp;
 
+struct SoundSystem {
+    time: Time,
+    root_ug: AUnit,
+}
+
+impl SoundSystem {
+    pub fn new(time: Time, ug: AUnit) -> SoundSystem {
+        SoundSystem {
+            time: time,
+            root_ug: ug,
+        }
+    }
+
+    pub fn run(&mut self, ad: &AudioDevice) {
+        ad.run(|mut buffer| {
+            let mut iter = buffer.iter_mut();
+            loop {
+                let (l, r) = self.root_ug.0.lock().unwrap().proc(&self.time);
+                match iter.next() {
+                    Some(lref) => *lref = l as f32,
+                    None => break,
+                }
+                match iter.next() {
+                    Some(rref) => *rref = r as f32,
+                    None => break,
+                }
+                self.time.inc();
+            }
+        });
+    }
+}
+
 fn main() {
     let sample_rate = 44100u32;
-    let audio_device = AudioDevice::open(sample_rate);
-
     let time = Time::new(sample_rate, 120.0);
-    let mut env = Env::init(time);
 
+    let mut env = Env::init(time);
     let s = r"
 (def $pat1 (pat (c4 1) (d 1) (e 1) (f 1) (g 1) (a 1) (b 1) (c5 1) (r 4)
                  loop))
@@ -36,8 +66,7 @@ fn main() {
 
 (+ (gain 0.3 (seq $pat1 $osc1 $eg1))
    (gain 0.25 (seq $pat2 $osc2 $eg2))
-   (seq $pat1 (wavetable $table (phase (saw 0 220))) $eg1))
-".to_string();
+   (seq $pat1 (wavetable $table (phase (saw 0 220))) $eg1))".to_string();
     let unit_graph = match tlisp::eval_all(tlisp::read(s).unwrap(), &mut env) {
         Ok(Value::Unit(ug)) => ug,
         Ok(_v) => panic!("Oh, unit graph is not a unit!!"),
@@ -45,20 +74,8 @@ fn main() {
     };
     println!("{}", tlisp::dump(unit_graph.clone()));
 
+    let mut lcd = SoundSystem::new(env.time, unit_graph);
 
-    audio_device.run(|mut buffer| {
-        let mut iter = buffer.iter_mut();
-        loop {
-            let (l, r) = unit_graph.0.lock().unwrap().proc(&env.time);
-            match iter.next() {
-                Some(lref) => *lref = l as f32,
-                None => break,
-            }
-            match iter.next() {
-                Some(rref) => *rref = r as f32,
-                None => break,
-            }
-            env.time.inc();
-        }
-    });
+    let audio_device = AudioDevice::open(lcd.time.sample_rate);
+    lcd.run(&audio_device);
 }
