72f7d2c85246ec1dd00f7f6e2ea0a3742733760e
t-sin <shinichi.tanaka45@gmail.com>
2019-12-09 00:47:31 +0900

    WIP: Osc

---
+++ src/ugen/osc.rs
@@ -0,0 +1,491 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use rand::{Rng, SeedableRng};
+use rand::rngs::SmallRng;
+
+use super::super::mtime::{Pos, Time, Clock};
+use super::core::{Signal, Param, Dump, Walk, UG, UGen, Aug, Proc, Osc};
+use super::misc::{Clip, Gain, Offset};
+
+pub struct Rand {
+    rng: SmallRng,
+    v: f64,
+}
+
+impl Rand {
+    pub fn new(seed: u64) -> Aug {
+        Aug::new(UGen::new(UG::Osc(
+            Box::new(Rand {
+                rng: SmallRng::seed_from_u64(seed),
+                v: 0.15,
+            })
+        )))
+    }
+}
+
+impl Walk for Rand {
+    fn walk(&self, _f: &mut dyn Fn(&Aug) -> bool) {}
+}
+
+impl Dump for Rand {
+    fn dump(&self, _shared_ug: &Vec<Aug>) -> Param {
+        let mut pnames = Vec::new();
+        let mut pvals = Vec::new();
+
+        pnames.push("v".to_string());
+        pvals.push(Box::new(Param::Value(self.v)));
+        Param::Ug("rand".to_string(), pnames, pvals)
+    }
+}
+
+impl Proc for Rand {
+    fn proc(&mut self, _time: &Time) -> Signal {
+        self.v = self.rng.gen();
+        (self.v, self.v)
+    }
+}
+
+impl Osc for Rand {
+    fn set_freq(&mut self, _u: Aug) {}
+}
+
+pub struct Sine {
+    pub init_ph: Aug,
+    pub ph: f64,
+    pub freq: Aug,
+}
+
+impl Sine {
+    pub fn new(init_ph: Aug, freq: Aug) -> Aug {
+        Aug::new(UGen::new(UG::Osc(
+            Box::new(Sine { init_ph: init_ph, ph: 0.0, freq: freq })
+        )))
+    }
+}
+
+impl Walk for Sine {
+    fn walk(&self, f: &mut dyn Fn(&Aug) -> bool) {
+        if f(&self.init_ph) {
+            self.init_ph.walk(f);
+        }
+        if f(&self.freq){
+            self.freq.walk(f);
+        }
+    }
+}
+
+impl Dump for Sine {
+    fn dump(&self, shared_ug: &Vec<Aug>) -> Param {
+        let mut pnames = Vec::new();
+        let mut pvals = Vec::new();
+
+        pnames.push("init_ph".to_string());
+        match shared_ug.iter().position(|e| *e == self.init_ph) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.init_ph.dump(shared_ug))),
+        }
+
+        pnames.push("freq".to_string());
+        match shared_ug.iter().position(|e| *e == self.freq) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.freq.dump(shared_ug))),
+        }
+
+        Param::Ug("sine".to_string(), pnames, pvals)
+    }
+}
+
+impl Proc for Sine {
+    fn proc(&mut self, time: &Time) -> Signal {
+        let init_ph = self.init_ph.proc(&time).0;
+        let v = (init_ph + self.ph).sin();
+        let ph_diff = time.sample_rate as f64 / std::f64::consts::PI;
+        self.ph += self.freq.proc(&time).0 / ph_diff;
+
+        (v, v)
+    }
+}
+
+impl Osc for Sine {
+    fn set_freq(&mut self, u: Aug) {
+        self.freq = u;
+    }
+}
+
+pub struct Tri {
+    pub init_ph: Aug,
+    pub ph: f64,
+    pub freq: Aug,
+}
+
+impl Tri {
+    pub fn new(init_ph: Aug, freq: Aug) -> Aug {
+        Aug::new(UGen::new(UG::Osc(
+            Box::new(Tri { init_ph: init_ph, ph: 0.0, freq: freq })
+        )))
+    }
+}
+
+impl Walk for Tri {
+    fn walk(&self, f: &mut dyn Fn(&Aug) -> bool) {
+        if f(&self.init_ph) {
+            self.init_ph.walk(f);
+        }
+        if f(&self.freq) {
+            self.freq.walk(f);
+        }
+    }
+}
+
+impl Dump for Tri {
+    fn dump(&self, shared_ug: &Vec<Aug>) -> Param {
+        let mut pnames = Vec::new();
+        let mut pvals = Vec::new();
+
+        pnames.push("init_ph".to_string());
+        match shared_ug.iter().position(|e| *e == self.init_ph) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.init_ph.dump(shared_ug))),
+        }
+
+        pnames.push("freq".to_string());
+        match shared_ug.iter().position(|e| *e == self.freq) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.freq.dump(shared_ug))),
+        }
+
+        Param::Ug("tri".to_string(), pnames, pvals)
+    }
+}
+
+impl Proc for Tri {
+    fn proc(&mut self, time: &Time) -> Signal {
+        let ph = self.init_ph.proc(&time).0 + self.ph;
+
+        let ph_diff = time.sample_rate as f64 * 2.0;
+        self.ph += self.freq.proc(&time).0 / ph_diff;
+
+        let x = ph % 1.0;
+        let v;
+        if x >= 3.0 / 4.0 {
+            v = 4.0 * x - 4.0;
+        } else if x >= 1.0 / 4.0 && x < 3.0 / 4.0 {
+            v = -4.0 * x + 2.0;
+        } else {
+            v = 4.0 * x;
+        }
+        (v, v)
+    }
+}
+
+impl Osc for Tri {
+    fn set_freq(&mut self, u: Aug) {
+        self.freq = u;
+    }
+}
+
+pub struct Saw {
+    pub init_ph: Aug,
+    pub ph: f64,
+    pub freq: Aug,
+}
+
+impl Saw {
+    pub fn new(init_ph: Aug, freq: Aug) -> Aug {
+        Aug::new(UGen::new(UG::Osc(
+            Box::new(Saw { init_ph: init_ph, ph: 0.0, freq: freq })
+        )))
+    }
+}
+
+impl Walk for Saw {
+    fn walk(&self, f: &mut dyn Fn(&Aug) -> bool) {
+        if f(&self.init_ph) {
+            self.init_ph.walk(f);
+        }
+        if f(&self.freq) {
+            self.freq.walk(f);
+        }
+    }
+}
+
+impl Dump for Saw {
+    fn dump(&self, shared_ug: &Vec<Aug>) -> Param {
+        let mut pnames = Vec::new();
+        let mut pvals = Vec::new();
+
+        pnames.push("init_ph".to_string());
+        match shared_ug.iter().position(|e| *e == self.init_ph) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.init_ph.dump(shared_ug))),
+        }
+
+        pnames.push("freq".to_string());
+        match shared_ug.iter().position(|e| *e == self.freq) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.freq.dump(shared_ug))),
+        }
+
+        Param::Ug("saw".to_string(), pnames, pvals)
+    }
+}
+
+impl Proc for Saw {
+    fn proc(&mut self, time: &Time) -> Signal {
+        let ph = self.init_ph.proc(&time).0 + self.ph;
+        let ph_diff = time.sample_rate as f64 * 2.0;
+        self.ph += self.freq.proc(&time).0 / ph_diff;
+
+        let x = ph % 1.0;
+        let v;
+        if x >= 1.0 / 2.0 {
+            v = 2.0 * x - 2.0;
+        } else {
+            v = 2.0 * x;
+        }
+        (v, v)
+    }
+}
+
+impl Osc for Saw {
+    fn set_freq(&mut self, u: Aug) {
+        self.freq = u;
+    }
+}
+
+pub struct Pulse {
+    pub init_ph: Aug,
+    pub ph: f64,
+    pub freq: Aug,
+    pub duty: Aug,
+}
+
+impl Pulse {
+    pub fn new(init_ph: Aug, freq: Aug, duty: Aug) -> Aug {
+        Aug::new(UGen::new(UG::Osc(
+            Box::new(Pulse { init_ph: init_ph, ph: 0.0, freq: freq, duty: duty})
+        )))
+    }
+}
+
+impl Walk for Pulse {
+    fn walk(&self, f: &mut dyn Fn(&Aug) -> bool) {
+        if f(&self.init_ph) { self.init_ph.walk(f); }
+        if f(&self.freq) { self.freq.walk(f); }
+        if f(&self.duty) { self.duty.walk(f); }
+    }
+}
+
+impl Dump for Pulse {
+    fn dump(&self, shared_ug: &Vec<Aug>) -> Param {
+        let mut pnames = Vec::new();
+        let mut pvals = Vec::new();
+
+        pnames.push("init_ph".to_string());
+        match shared_ug.iter().position(|e| *e == self.init_ph) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.init_ph.dump(shared_ug))),
+        }
+
+        pnames.push("freq".to_string());
+        match shared_ug.iter().position(|e| *e == self.freq) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.freq.dump(shared_ug))),
+        }
+
+        pnames.push("duty".to_string());
+        match shared_ug.iter().position(|e| *e == self.duty) {
+            Some(n) => pvals.push(Box::new(Param::Shared(shared_ug.iter().nth(n).unwrap().clone()))),
+            None => pvals.push(Box::new(self.duty.dump(shared_ug))),
+        }
+
+        Param::Ug("pulse".to_string(), pnames, pvals)
+    }
+}
+
+impl Proc for Pulse {
+    fn proc(&mut self, time: &Time) -> Signal {
+        let ph = self.init_ph.proc(&time).0 + self.ph;
+        let duty = self.duty.proc(&time).0;
+        let ph_diff = time.sample_rate as f64 * 2.0;
+        self.ph += self.freq.proc(&time).0 / ph_diff;
+
+        let x = ph % 1.0;
+        let v;
+        if x < duty {
+            v = 1.0;
+        } else {
+            v = -1.0;
+        }
+        (v, v)
+    }
+}
+
+impl Osc for Pulse {
+    fn set_freq(&mut self, u: Aug) {
+        self.freq = u;
+    }
+}
+
+pub struct Phase {
+    pub root: Aug,
+    pub osc: Aug,
+}
+
+// impl Phase {
+//     pub fn new(u: Aug) -> Aug {
+//         Mut::amut(ProcGraph::new(Node::Osc(
+//             Mut::amut(Phase {
+//                 root: Mut::amut(ProcGraph::new(Node::Sig(Mut::amut(Offset {
+//                     v: 1.0,
+//                     src: Mut::amut(ProcGraph::new(Node::Sig(
+//                         Mut::amut(Gain {
+//                             v: 0.5,
+//                             src: Mut::amut(ProcGraph::new(Node::Sig(
+//                                 Mut::amut(Clip {
+//                                     min: -1.0, max: 1.0, src: u.clone(),
+//                                 })
+//                             ))),
+//                         })
+//                     ))),
+//                 })))),
+//                 osc: u.clone(),
+//             })
+//         )))
+//     }
+// }
+
+// impl Walk for Phase {
+//     fn walk(&self, f: &mut FnMut(&Aug) -> bool) {
+//         if f(&self.osc) { self.osc.0.lock().unwrap().walk(f); }
+//     }
+// }
+
+// impl Dump for Phase {
+//     fn dump(&self, shared_ug: &Vec<Aug>, shared_map: &HashMap<usize, String>) -> Param {
+//         let mut pnames = Vec::new();
+//         let mut pvals = Vec::new();
+
+//         pnames.push("osc".to_string());
+//         match shared_ug.iter().position(|e| Arc::ptr_eq(e, &self.osc)) {
+//             Some(idx) => pvals.push(Box::new(Param::Value(shared_map.get(&idx).unwrap().to_string()))),
+//             None => pvals.push(Box::new(self.osc.0.lock().unwrap().dump(shared_ug, shared_map))),
+//         }
+
+//         Param::Op("phase".to_string(), pnames, pvals)
+//     }
+// }
+
+// impl Proc for Phase {
+//     fn proc(&mut self, time: &Time) -> Signal {
+//         let v = self.root.0.lock().unwrap().proc(time);
+//         v
+//     }
+// }
+
+// impl Osc for Phase {
+//     fn set_freq(&mut self, freq: Aug) {
+//         if let Node::Osc(osc) = &self.osc.clone().0.lock().unwrap().node {
+//             osc.0.lock().unwrap().set_freq(freq);
+//         } else {
+//             self.osc = freq;
+//         }
+//     }
+// }
+
+// pub struct WaveTable {
+//     pub table: Aug,
+//     pub ph: Aug,
+// }
+
+// impl WaveTable {
+//     pub fn from_osc(osc: Aug, ph: Aug, time: &Time) -> Aug {
+//         let mut table = Vec::new();
+//         let table_len = 256;
+//         let mut time = Time {
+//             sample_rate: (table_len as f64 / 2.0) as u32,
+//             tick: 0,
+//             bpm: time.bpm,
+//             measure: time.measure.clone(),
+//             pos: Pos { bar: 0, beat: 0, pos: 0.0 },
+//         };
+//         for _i in 0..table_len {
+//             let v = osc.0.lock().unwrap().proc(&time).0;
+//             table.push(v);
+//             time.inc();
+//         }
+//         Mut::amut(ProcGraph::new(Node::Osc(
+//             Mut::amut(WaveTable {
+//                 table: Table::new(table),
+//                 ph: ph,
+//             })
+//         )))
+//     }
+
+//     pub fn from_table(table: Aug, ph: Aug) -> Aug {
+//         Mut::amut(ProcGraph::new(Node::Osc(
+//             Mut::amut(WaveTable {
+//                 table: table,
+//                 ph: ph,
+//             })
+//         )))
+//     }
+// }
+
+// fn linear_interpol(v1: f64, v2: f64, r: f64) -> f64 {
+//     let r = r % 1.0;
+//     v1 * r + v2 * (1.0 - r)
+// }
+
+// impl Walk for WaveTable {
+//     fn walk(&self, f: &mut FnMut(&Aug) -> bool) {
+//         if f(&self.table) { self.table.0.lock().unwrap().walk(f); }
+//         if f(&self.ph) { self.ph.0.lock().unwrap().walk(f); }
+//     }
+// }
+
+// impl Dump for WaveTable {
+//     fn dump(&self, shared_ug: &Vec<Aug>, shared_map: &HashMap<usize, String>) -> Param {
+//         let mut pnames = Vec::new();
+//         let mut pvals = Vec::new();
+
+//         pnames.push("table".to_string());
+//         match shared_ug.iter().position(|e| Arc::ptr_eq(e, &self.table)) {
+//             Some(idx) => pvals.push(Box::new(Param::Value(shared_map.get(&idx).unwrap().to_string()))),
+//             None => pvals.push(Box::new(self.table.0.lock().unwrap().dump(shared_ug, shared_map))),
+//         }
+
+//         pnames.push("ph".to_string());
+//         match shared_ug.iter().position(|e| Arc::ptr_eq(e, &self.ph)) {
+//             Some(idx) => pvals.push(Box::new(Param::Value(shared_map.get(&idx).unwrap().to_string()))),
+//             None => pvals.push(Box::new(self.ph.0.lock().unwrap().dump(shared_ug, shared_map))),
+//         }
+
+//         Param::Op("wavetable".to_string(), pnames, pvals)
+//     }
+// }
+
+// impl Proc for WaveTable {
+//     fn proc(&mut self, time: &Time) -> Signal {
+//         if let Node::Tab(t) = &self.table.0.lock().unwrap().node {
+//             let table = &t.0.lock().unwrap().0;
+//             let len = table.len() as f64;
+//             let p = self.ph.0.lock().unwrap().proc(&time).0 * len;
+//             let pos1 = (p.floor() % len) as usize;
+//             let pos2 = (p.ceil() % len) as usize;
+//             let v = linear_interpol(table[pos1], table[pos2], p.fract());
+//             (v, v)
+//         } else {
+//             panic!("it's not a table!!")
+//         }
+//     }
+// }
+
+// impl Osc for WaveTable {
+//     fn set_freq(&mut self, freq: Aug) {
+//         if let Node::Osc(osc) = &self.ph.0.lock().unwrap().node {
+//             osc.0.lock().unwrap().set_freq(freq);
+//         }
+//     }
+// }
